e0f51bee7fd077877d4d060406f80ace00cdad77
pollockcl <pollock.clee@gmail.com>
2018-06-23 18:26:00 -0600

    the form functions!

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  // your code goes here.
+    var selector = $("input[name='selector']");
+    var property = $("input[name='property']");
+    var value    = $("input[name='value']");
+
+    changeStyle(selector, property, value);
 });
+
+const changeStyle = (selector, property, value) => {
+    $('#style_editor').submit(function() {
+        $(selector.val()).css(property.val(), value.val());
+        event.preventDefault();
+    });
+};
+
